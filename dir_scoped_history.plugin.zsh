28f9a8bc9120408888ef688648e9c4ee7cbabda5
Andy Park <andy@bigbearlabs.com>
2019-03-24 22:17:28 +0100

    hook into `cd`.

--- dir_scoped_history.plugin.zsh
+++ dir_scoped_history.plugin.zsh
@@ -5,3 +5,8 @@ setopt HIST_SAVE_NO_DUPS INC_APPEND_HISTORY
 
 # set history file to `cwd`/.history.
 HISTFILE=${PWD}/.history
+
+# hook into `cd` and load the history.
+chpwd() {
+  [[ $PWD = $OLDPWD ]] || fc -Pp ${PWD}/.history
+}
